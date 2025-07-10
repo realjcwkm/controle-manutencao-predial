d34b86c686f7c11791f2539fb361af8078c362b5
joaomonteandrade <joaomontea@gmail.com>
2025-07-10 20:58:18 -0300

    Feat: Adicionar a classe base Controller

---
+++ ControleManutencaoPredial/src/br/com/loggers/controller/Controller.java
@@ -0,0 +1,17 @@
+
+package br.com.loggers.controller;
+
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+
+
+public class Controller {
+    
+    public Controller() {
+    }
+    
+}
