0e671ca5fe0ade23409fc8f8a3e5c6d33d9c742d
joaomonteandrade <joaomontea@gmail.com>
2025-07-10 20:24:01 -0300

    Chore: Adicionando driver do MySQL e a classe de conexão

---
+++ ControleManutencaoPredial/src/br/com/loggers/model/Connect.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.loggers.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Connect {
+    private Connection con;
+}
