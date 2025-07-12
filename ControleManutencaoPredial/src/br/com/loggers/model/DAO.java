b5132b9b42eb89d70da22de3bfa78dcc9a664b0c
guiguik12 <guigui_pvh@hotmail.com>
2025-07-11 22:36:55 -0400

    Feat: Implementando funcionalidade de registro de logs

--- ControleManutencaoPredial/src/br/com/loggers/model/DAO.java
+++ ControleManutencaoPredial/src/br/com/loggers/model/DAO.java
@@ -352,3 +352,20 @@ public class DAO {
             return false;
         }
     }
+    public boolean insertLog(Log log) {
+        String sql = "insert into log(data, hora, tipo, descricao, tipo_log_id_tipo_log, usuario_id_usuario) values (?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = new Connect().conectar(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setDate(1, java.sql.Date.valueOf(log.getData()));
+            stmt.setTime(2, java.sql.Time.valueOf(log.getHora()));
+            stmt.setInt(3, log.getTipo());
+            stmt.setString(4, log.getDescricao());
+            stmt.setInt(5, log.getTipo_log_id_tipo_log());
+            stmt.setInt(6, log.getUsuario_id_usuario());
+            stmt.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
