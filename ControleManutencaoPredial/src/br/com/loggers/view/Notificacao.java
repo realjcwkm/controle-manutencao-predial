77d47f8524bfaac4e72ac45436bdf39119d6c95d
guiguik12 <guigui_pvh@hotmail.com>
2025-07-11 23:28:34 -0400

    Feat: Adicionando o método de renderização da janela

--- ControleManutencaoPredial/src/br/com/loggers/view/Notificacao.java
+++ ControleManutencaoPredial/src/br/com/loggers/view/Notificacao.java
@@ -34,6 +34,17 @@ public class Notificacao extends javax.swing.JPanel {
         setOpaque(false);
     }
     
+    
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        
+        Graphics2D g2 = (Graphics2D) grphcs.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fill(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 15, 15));
+        g2.dispose();
+        super.paintComponent(grphcs);
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
