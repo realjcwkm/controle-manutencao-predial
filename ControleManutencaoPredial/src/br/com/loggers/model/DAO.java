9468e428569d237ef01bfbc378dfdbb46d857b09
guiguik12 <guigui_pvh@hotmail.com>
2025-07-11 21:56:00 -0400

    Feat: Implementando contador para ordens de serviço agendadas

--- ControleManutencaoPredial/src/br/com/loggers/model/DAO.java
+++ ControleManutencaoPredial/src/br/com/loggers/model/DAO.java
@@ -96,3 +96,19 @@ public class DAO {
         System.out.println(count);
         return count;
     }
+
+    public int countAgendado() {
+        int count = 0;
+        String sql = "SELECT COUNT(*) AS total FROM ordem_servico WHERE status = 'agendado'";
+        try (Connection conn = new Connect().conectar(); PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            if (rs.next()) {
+                count = rs.getInt("total");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(count);
+        return count;
+    }
