e9f41b10aa8004371dafcdefac1ea1958628558b
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 20:42:56 -0400

    Feat: Adicionando atributos base da classe Usuario

---
+++ ControleManutencaoPredial/src/br/com/loggers/controller/User.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.loggers.controller;
+
+/**
+ *
+ * @author Matheus
+ */
+
+public class User {
+    String nome;
+    String email;
+    String senha;
+    int id_usuario;
+    int tipo;
+}
