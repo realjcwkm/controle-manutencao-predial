86220cdbac43af1537d2c36d06bca101c89714b9
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 21:15:10 -0400

    Feat: Adicionando o DAO ao controller para operações com o Banco de Dados

--- ControleManutencaoPredial/src/br/com/loggers/controller/Controller.java
+++ ControleManutencaoPredial/src/br/com/loggers/controller/Controller.java
@@ -1,6 +1,10 @@
 
 package br.com.loggers.controller;
 
+// Importações das classes e bibliotecas necessárias
+import br.com.loggers.model.Connect;
+import br.com.loggers.model.DAO;
+
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.util.List;
@@ -11,7 +15,15 @@ import javax.swing.table.TableModel;
 
 public class Controller {
     
+    // Objeto que gerencia as operações com o banco de dados
+    private DAO theDAO;
+
+    // Construtor da classe Controller
+    // Responsável por inicializar a instância do DAO
     public Controller() {
+        System.out.println("[Controller] Inicializando instância do Controller...");
+        this.theDAO = new DAO();
+        System.out.println("[Controller] DAO instanciado com sucesso.");
     }
-    
+
 }
