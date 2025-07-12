3a3de1a65db95bf87f651ad1de161ac2355cb8f1
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 20:52:45 -0400

    Feat: Adicionando construtor da classe Manutencao

--- ControleManutencaoPredial/src/br/com/loggers/controller/Manutencao.java
+++ ControleManutencaoPredial/src/br/com/loggers/controller/Manutencao.java
@@ -9,11 +9,16 @@ package br.com.loggers.controller;
  * @author Matheus
  */
 public class Manutencao {
-    
     String tipo_manutencao;
     int ordem_servico_id_ordem_servico;
     int local_id_local;
     int periodicidade_id_periodicidade;
 
+    public Manutencao(String tipo_manutencao, int ordem_servico_id_ordem_servico, int local_id_local, int periodicidade_id_periodicidade) {
+        this.tipo_manutencao = tipo_manutencao;
+        this.ordem_servico_id_ordem_servico = ordem_servico_id_ordem_servico;
+        this.local_id_local = local_id_local;
+        this.periodicidade_id_periodicidade = periodicidade_id_periodicidade;
+    }
     
 }
