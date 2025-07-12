874e19dca6832baccc54dd8c17661ea3353e4a54
eduardars-ifro <eduardaleigue@gmail.com>
2025-07-11 20:45:52 -0400

    Feat: Implementando conversão da data de intalação e manutenção em extenso para numeral

--- ControleManutencaoPredial/src/br/com/loggers/view/AtivoForms.java
+++ ControleManutencaoPredial/src/br/com/loggers/view/AtivoForms.java
@@ -53,6 +53,23 @@ public class AtivoForms extends javax.swing.JPanel {
         }
     }
     
+    public static int converterMes(String mes) {
+    switch (mes.toLowerCase()) {
+        case "janeiro": return 1;
+        case "fevereiro": return 2;
+        case "março": return 3;
+        case "abril": return 4;
+        case "maio": return 5;
+        case "junho": return 6;
+        case "julho": return 7;
+        case "agosto": return 8;
+        case "setembro": return 9;
+        case "outubro": return 10;
+        case "novembro": return 11;
+        case "dezembro": return 12;
+        default: return -1;
+    }
+}
     
 
     /**
