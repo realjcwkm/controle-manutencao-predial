4d4f0242b13ea4598c77c71365fb89b8b55aceed
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 20:57:16 -0400

    Feat: Adicionando atributos base da classe Ativo

---
+++ ControleManutencaoPredial/src/br/com/loggers/controller/Ativo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.loggers.controller;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Ativo {
+    String modelo_ativo, descricao;
+    LocalDate data_instalacao;
+    LocalDate ultima_manutencao;
+    int local_id_local;
+    int tipo_ativo_id_tipo_ativo;
+    int periodicidade_id_periodicidade;
+
+    
+}
