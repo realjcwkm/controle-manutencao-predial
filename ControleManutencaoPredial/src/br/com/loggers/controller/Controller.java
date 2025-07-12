21fba12428e7972e555ffb332d89830e5676014d
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 21:34:36 -0400

    Chore: Integrando o cadastro do usuário e a validação no controller com o método para inserir usuário no Banco de Dados

--- ControleManutencaoPredial/src/br/com/loggers/controller/Controller.java
+++ ControleManutencaoPredial/src/br/com/loggers/controller/Controller.java
@@ -1,9 +1,10 @@
-
 package br.com.loggers.controller;
 
 // Importações das classes e bibliotecas necessárias
 import br.com.loggers.model.Connect;
 import br.com.loggers.model.DAO;
+import br.com.loggers.view.View;
+import br.com.loggers.controller.User;
 
 import java.sql.SQLException;
 import java.sql.Connection;
@@ -24,6 +25,28 @@ public class Controller {
         System.out.println("[Controller] Inicializando instância do Controller...");
         this.theDAO = new DAO();
         System.out.println("[Controller] DAO instanciado com sucesso.");
+    }
+
+    // Método responsável por criar um novo usuário no sistema
+    public boolean createUser(User theUser) throws SQLException {
+        System.out.println("[createUser] Iniciando processo de criação de usuário...");
+
+        // Verifica se o usuário fornecido é válido
+        if (theUser == null) {
+            System.err.println("[createUser] Objeto User é nulo. Encerrando operação.");
+            return false;
+        }
+
+        // Exibe os dados do usuário para fins de verificação
+        System.out.println("[createUser] Nome: " + theUser.getNome());
+        System.out.println("[createUser] Email: " + theUser.getEmail());
+
+        // Chama o método DAO para inserir o usuário
+        boolean sucesso = this.theDAO.createUser(theUser);
+
+        // Exibe o resultado da operação
+        System.out.println("[createUser] Resultado da operação: " + sucesso);
+        return sucesso;
     }
 
 }
