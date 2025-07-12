98df1d9653fe999852cb601a603352e9d41ee8be
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 20:46:15 -0400

    Feat: Adicionando atributos base da classe OS

---
+++ ControleManutencaoPredial/src/br/com/loggers/controller/OS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.loggers.controller;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Matheus
+ */
+public class OS {
+
+    String titulo;
+    String urgencia;
+    String status;
+    String descricao;
+    LocalDate prazo;
+    int tecnico;
+    int local_id_local;
+    
+    
+}
