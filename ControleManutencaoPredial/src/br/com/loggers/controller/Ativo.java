29410d4ec77c9369034d20dc3d68a00335c14e93
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 20:57:32 -0400

    Feat: Adicionando construtor da classe Ativo

--- ControleManutencaoPredial/src/br/com/loggers/controller/Ativo.java
+++ ControleManutencaoPredial/src/br/com/loggers/controller/Ativo.java
@@ -18,5 +18,14 @@ public class Ativo {
     int tipo_ativo_id_tipo_ativo;
     int periodicidade_id_periodicidade;
 
-    
+    public Ativo(String modelo_ativo, String descricao, LocalDate data_instalacao, LocalDate ultima_manutencao, int local_id_local, int tipo_ativo_id_tipo_ativo, int periodicidade_id_periodicidade) {
+        this.modelo_ativo = modelo_ativo;
+        this.descricao = descricao;
+        this.data_instalacao = data_instalacao;
+        this.ultima_manutencao = ultima_manutencao;
+        this.local_id_local = local_id_local;
+        this.tipo_ativo_id_tipo_ativo = tipo_ativo_id_tipo_ativo;
+        this.periodicidade_id_periodicidade = periodicidade_id_periodicidade;
+    }
+
 }
