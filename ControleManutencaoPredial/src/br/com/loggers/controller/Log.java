d47e3b0d7e223ca27e7f8f705b4e3d7038c83b58
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 20:54:59 -0400

    Feat: Adicionando atributos base da classe Log

---
+++ ControleManutencaoPredial/src/br/com/loggers/controller/Log.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.loggers.controller;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Log {
+    
+    LocalDate data;
+    LocalTime hora;
+    int tipo, tipo_log_id_tipo_log;
+    int usuario_id_usuario;
+    int ordem_servico_id_ordem_servico;
+    int manutencao_id_manutencao;
+    String descricao;
+
+    
+}
