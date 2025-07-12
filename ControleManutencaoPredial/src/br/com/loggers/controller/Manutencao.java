7e2b2ac0823378128c0aadc6101addb639407328
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 20:50:02 -0400

    Feat: Adicionando atributos base da classe Manutencao

---
+++ ControleManutencaoPredial/src/br/com/loggers/controller/Manutencao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.loggers.controller;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Manutencao {
+    
+    String tipo_manutencao;
+    int ordem_servico_id_ordem_servico;
+    int local_id_local;
+    int periodicidade_id_periodicidade;
+
+    
+}
