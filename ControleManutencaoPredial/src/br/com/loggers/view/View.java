1b26587a55ca2d2d2f02a112d12d753b307d7e6f
joaomonteandrade <joaomontea@gmail.com>
2025-07-11 22:56:30 -0300

    Feat: Implementando método para ir para a tela de cadastro

--- ControleManutencaoPredial/src/br/com/loggers/view/View.java
+++ ControleManutencaoPredial/src/br/com/loggers/view/View.java
@@ -1713,6 +1713,10 @@ public class View extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        CardLayout card = (CardLayout) root.getLayout();
+        card.show(root, "cadastro");
+    }//GEN-LAST:event_jButton2ActionPerformed
     
     /**
      * @param args the command line arguments
