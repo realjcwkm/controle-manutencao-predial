0f9d6102ff05b4e8344650067e545d9777e35933
Matheus <matheushenriquesl06@gmail.com>
2025-07-11 20:55:25 -0400

    Feat: Adicionando construtor da classe Log

--- ControleManutencaoPredial/src/br/com/loggers/controller/Log.java
+++ ControleManutencaoPredial/src/br/com/loggers/controller/Log.java
@@ -12,7 +12,6 @@ import java.time.LocalTime;
  * @author Matheus
  */
 public class Log {
-    
     LocalDate data;
     LocalTime hora;
     int tipo, tipo_log_id_tipo_log;
@@ -20,6 +19,17 @@ public class Log {
     int ordem_servico_id_ordem_servico;
     int manutencao_id_manutencao;
     String descricao;
+
+    public Log(LocalDate data, LocalTime hora, int tipo, int tipo_log_id_tipo_log, int usuario_id_usuario, int ordem_servico_id_ordem_servico, int manutencao_id_manutencao, String descricao) {
+        this.data = data;
+        this.hora = hora;
+        this.tipo = tipo;
+        this.tipo_log_id_tipo_log = tipo_log_id_tipo_log;
+        this.usuario_id_usuario = usuario_id_usuario;
+        this.ordem_servico_id_ordem_servico = ordem_servico_id_ordem_servico;
+        this.manutencao_id_manutencao = manutencao_id_manutencao;
+        this.descricao = descricao;
+    }
 
     
 }
