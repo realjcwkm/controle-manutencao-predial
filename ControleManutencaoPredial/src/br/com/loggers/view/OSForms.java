c37b4d1da69328eb6a3534e1d638bb470e18e5c1
eduardars-ifro <eduardaleigue@gmail.com>
2025-07-11 20:15:31 -0400

    Feat: Implementando método para converter o mês em extenso para mês numeral

--- ControleManutencaoPredial/src/br/com/loggers/view/OSForms.java
+++ ControleManutencaoPredial/src/br/com/loggers/view/OSForms.java
@@ -63,7 +63,22 @@ public class OSForms extends javax.swing.JPanel {
     return true;
     }
     
-    
+    public static int converterMes(String mes) {
+    switch (mes.toLowerCase()) {
+        case "janeiro": return 1;
+        case "fevereiro": return 2;
+        case "março": return 3;
+        case "abril": return 4;
+        case "maio": return 5;
+        case "junho": return 6;
+        case "julho": return 7;
+        case "agosto": return 8;
+        case "setembro": return 9;
+        case "outubro": return 10;
+        case "novembro": return 11;
+        case "dezembro": return 12;
+        default: return -1;
+    }
 }
     
 
